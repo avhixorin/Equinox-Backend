1b195ab9707e31376fa5ad3bea8cce5914f44bcb
avhixorin <avhixorin@gmail.com>
2025-08-02 15:03:06 +0530

    truncated content for model predict route

--- cron/src/util/updateLongDescription.ts
+++ cron/src/util/updateLongDescription.ts
@@ -37,15 +37,21 @@ export async function getNewsFullArticleAndSetBias(miniNews: { id: string; link:
 
     if (article?.textContent) {
 
-      const cleaned = article.textContent
+     const cleaned = article.textContent
         .replace(/(?:twitter\.com|facebook\.com|instagram\.com)[^\s]+/gi, "")
         .replace(/(First Published|Last Updated):.+?\n/gi, "")
         .replace(/\s{2,}/g, " ")
 
+      const maxSafeTokenLimit = 1024;
+      const avgCharsPerToken = 2;
+      const safeCharLimit = maxSafeTokenLimit * avgCharsPerToken; 
+
+      const truncatedContent = cleaned.slice(0, safeCharLimit);
+
 
       try {
         const response = await axios.post("https://news-bias-service-696524841053.us-central1.run.app/predict", {
-          content: cleaned
+          content: truncatedContent
         }, {
           headers: {
             "Content-Type": "application/json",
